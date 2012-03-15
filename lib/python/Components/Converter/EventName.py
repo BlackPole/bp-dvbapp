d618b95a7789622c984d2deedd5b33a6e57675da
meo <lupomeo@gmail.com>
2012-03-15 21:46:59 +0100

    dd next event name & description in channel list

--- lib/python/Components/Converter/EventName.py
+++ lib/python/Components/Converter/EventName.py
@@ -1,5 +1,6 @@
 from Components.Converter.Converter import Converter
 from Components.Element import cached
+from enigma import eEPGCache
 
 class EventName(Converter, object):
 	NAME = 0
@@ -7,6 +8,8 @@ class EventName(Converter, object):
 	EXTENDED_DESCRIPTION = 2
 	FULL_DESCRIPTION = 3
 	ID = 4
+	NEXT_NAME = 5
+        NEXT_DESCRIPTION = 6
 	
 	def __init__(self, type):
 		Converter.__init__(self, type)
@@ -18,6 +21,10 @@ class EventName(Converter, object):
 			self.type = self.FULL_DESCRIPTION
 		elif type == "ID":
 			self.type = self.ID
+		elif type == "NextName":
+                        self.type = self.NEXT_NAME
+                elif type == "NextDescription":
+                        self.type = self.NEXT_DESCRIPTION
 		else:
 			self.type = self.NAME
 
@@ -41,5 +48,18 @@ class EventName(Converter, object):
 			return description + extended
 		elif self.type == self.ID:
 			return str(event.getEventId())
+		elif self.type == self.NEXT_NAME or self.type == self.NEXT_DESCRIPTION:
+                        reference = self.source.service
+                        info = reference and self.source.info
+                        if info is not None:
+                        	nextEvent = self.epgcache.lookupEvent(['SETX', (reference.toString(), 1, -1)])
+                                if self.type == self.NEXT_NAME:
+                                        return nextEvent[0][2]
+                                else:
+                                        if nextEvent[0][1] != "":
+                                                return nextEvent[0][1]
+                                        else:
+                                                return nextEvent[0][0]
+		return ""
 		
 	text = property(getText)
