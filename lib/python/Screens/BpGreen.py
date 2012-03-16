e913e2fdb1cfeeb3d083730221296c0a38d86bde
meo <lupomeo@gmail.com>
2012-03-16 16:20:58 +0100

    Complete Green Panel !

--- lib/python/Screens/BpGreen.py
+++ lib/python/Screens/BpGreen.py
@@ -3,6 +3,7 @@ from Screens.Standby import TryQuitMainloop
 from Screens.MessageBox import MessageBox
 from Screens.Console import Console
 from Components.ActionMap import ActionMap
+from Components.config import config, ConfigText, configfile
 from Components.Sources.List import List
 from Components.Label import Label
 from Components.PluginComponent import plugins
@@ -12,6 +13,8 @@ from Plugins.Plugin import PluginDescriptor
 from Plugins.SystemPlugins.SoftwareManager.plugin import PacketManager, PluginManager
 from os import system, listdir, chdir, getcwd, remove as os_remove
 from enigma import eDVBDB
+
+config.misc.fast_plugin_button = ConfigText(default="")
 
 class DeliteGreenPanel(Screen):
 	def __init__(self, session):
@@ -25,10 +28,10 @@ class DeliteGreenPanel(Screen):
 		{
 			"ok": self.runPlug,
 			"back": self.close,
-			"red": self.notAval,
-			"green": self.notAval,
-			"yellow": self.addoNs,
-			"blue": self.notAval
+			"red": self.keyRed,
+			"green": self.keyGreen,
+			"yellow": self.keyYellow,
+			"blue": self.keyBlue
 		}, -1)
 			
 	def runPlug(self):
@@ -50,11 +53,74 @@ class DeliteGreenPanel(Screen):
 		
 		self["list"].list = self.list	
 	
-	def addoNs(self):
+	def keyYellow(self):
 		self.session.open(DeliteAddons)
+	
+	def keyRed(self):
+		self.session.open(DeliteSetupFp)
+		
+	def keyGreen(self):
+		runplug = None
+		for plugin in self.list:
+			if  plugin[3].name == config.misc.fast_plugin_button.value:
+				runplug = plugin[3]
+				break
+		
+		if runplug is not None:
+			runplug(session=self.session)
+		else:
+			self.session.open(MessageBox, "Fast Plugin not found. You have to setup Fast Plugin before to use this shortcut.", MessageBox.TYPE_INFO)
 
-	def notAval(self):
-		self.session.open(MessageBox, "Sorry, function not available in Black Pole", MessageBox.TYPE_INFO)
+	def keyBlue(self):
+		self.session.open(DeliteScript)
+
+class DeliteSetupFp(Screen):
+	skin = """
+	<screen position="160,115" size="390,370" title="Black Hole Fast Plugin Setup">
+		<widget source="list" render="Listbox" position="10,10" size="370,300" scrollbarMode="showOnDemand" >
+			<convert type="StringList" />
+		</widget>
+		<ePixmap pixmap="skin_default/buttons/red.png" position="115,320" size="140,40" alphatest="on" />
+		<widget name="key_red" position="115,320" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#9f1313" transparent="1" />
+	</screen>"""
+	
+	def __init__(self, session):
+		Screen.__init__(self, session)
+		
+		self["key_red"] = Label(_("Set Favourite"))
+		self.list = []
+		self["list"] = List(self.list)
+		self.updateList()
+		
+		self["actions"] = ActionMap(["WizardActions", "ColorActions"],
+		{
+			"ok": self.save,
+			"back": self.close,
+			"red": self.save
+		}, -1)
+			
+	def updateList(self):
+		self.list = [ ]
+		self.pluginlist = plugins.getPlugins(PluginDescriptor.WHERE_PLUGINMENU)
+		for plugin in self.pluginlist:
+			if plugin.icon is None:
+				png = LoadPixmap(resolveFilename(SCOPE_SKIN_IMAGE, "skin_default/icons/plugin.png"))
+			else:
+				png = plugin.icon
+			res = (plugin.name, plugin.description, png)
+			self.list.append(res)
+		
+		self["list"].list = self.list
+		
+	def save(self):
+		mysel = self["list"].getCurrent()
+		if mysel:
+			message = "Fast plugin set to: " + mysel[0] + "\nKey: 2x Green"
+			mybox = self.session.openWithCallback(self.close, MessageBox, message, MessageBox.TYPE_INFO)
+			mybox.setTitle(_("Configuration Saved"))
+			config.misc.fast_plugin_button.value = mysel[0]
+			config.misc.fast_plugin_button.save()
+			configfile.save()
 
 class DeliteAddons(Screen):
 	skin = """
@@ -332,6 +398,69 @@ class Nab_uninstPanel(Screen):
 			mybox = self.session.open(MessageBox, "Addon Succesfully Removed.", MessageBox.TYPE_INFO)
 			mybox.setTitle("Info")
 			self.close()
+
+class DeliteScript(Screen):
+	skin = """
+	<screen position="80,100" size="560,410" title="Black Hole Script Panel">
+		<widget source="list" render="Listbox" position="10,10" size="540,300" scrollbarMode="showOnDemand" >
+			<convert type="StringList" />
+		</widget>
+		<widget name="statuslab" position="10,320" size="540,30" font="Regular;16" valign="center" noWrap="1" backgroundColor="#333f3f3f" foregroundColor="#FFC000" shadowOffset="-2,-2" shadowColor="black" />
+		<ePixmap pixmap="skin_default/buttons/red.png" position="210,360" size="140,40" alphatest="on" />
+		<widget name="key_red" position="210,360" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#9f1313" transparent="1" />
+	</screen>"""
+	
+	def __init__(self, session):
+		Screen.__init__(self, session)
+		
+		self["statuslab"] = Label("N/A")
+		self["key_red"] = Label("Execute")
+		self.mlist = []
+		self.populateSL()
+		self["list"] = List(self.mlist)
+		self["list"].onSelectionChanged.append(self.schanged)
+		
+		self["actions"] = ActionMap(["WizardActions", "ColorActions"],
+		{
+			"ok": self.myGo,
+			"back": self.close,
+			"red": self.myGo
+		})
+		self.onLayoutFinish.append(self.refr_sel)
+		
+	def refr_sel(self):
+		self["list"].index = 1
+		self["list"].index = 0
+		
+	def populateSL(self):
+		myscripts = listdir("/usr/script")
+		for fil in myscripts:
+			if fil.find('.sh') != -1:
+				fil2 = fil[:-3]
+				desc = "N/A"
+				f = open("/usr/script/" + fil,'r')
+				for line in f.readlines():
+					if line.find('#DESCRIPTION=') != -1:
+						line = line.strip()
+						desc = line[13:]
+				f.close()
+				res = (fil2, desc)
+				self.mlist.append(res)			
+
+	def schanged(self):
+		mysel = self["list"].getCurrent()
+		if mysel:
+			mytext = " " + mysel[1]
+			self["statuslab"].setText(mytext)
+
+			
+	def myGo(self):
+		mysel = self["list"].getCurrent()
+		if mysel:
+			mysel = mysel[0]
+			mysel2 = "/usr/script/" + mysel + ".sh"
+			mytitle = "Black Hole E2 Script: " + mysel
+			self.session.open(Console, title=mytitle, cmdlist=[mysel2])
 
 class DeliteGp:
 	def __init__(self):
