b5cb4e1f27c31d3256fb480f81e52a3dbe8006b5
meo <lupomeo@gmail.com>
2012-03-23 13:44:32 +0100

    workaround ultimo vfd

--- lib/gdi/gpixmap.cpp
+++ lib/gdi/gpixmap.cpp
@@ -370,30 +370,7 @@ void gPixmap::blit(const gPixmap &src, const eRect &_pos, const gRegion &clip, i
 //		eDebug("srcarea after scale: %d %d %d %d",
 //			srcarea.x(), srcarea.y(), srcarea.width(), srcarea.height());
 
-		if (accel)
-		{
-			/* we have hardware acceleration for this blit operation */
-			if (flag & (blitAlphaTest | blitAlphaBlend))
-			{
-				/* alpha blending is requested */
-				if (!gAccel::getInstance()->hasAlphaBlendingSupport())
-				{
-					/* our hardware does not support alphablending */
-					if (flag & blitScale)
-					{
-						/* we have to scale, we really need hardware for that. Strip the alpha blending flags, and continue */
-						flag &= ~(blitAlphaTest | blitAlphaBlend);
-					}
-					else
-					{
-						/* we do not have to scale, so we will perform software alphablending */
-						accel = false;
-					}
-				}
-			}
-		}
-
-		if (accel)
+		if ((surface->data_phys && src.surface->data_phys) && (gAccel::getInstance()))
 			if (!gAccel::getInstance()->blit(surface, src.surface, area, srcarea, flag))
 				continue;
 
